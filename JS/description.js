92929768f4905b1a3fbd40574ba3fc346ce2d2e8
Leonardo Gutierrez <tilarian@outlook.com>
2023-07-27 16:22:22 -0300

    .

--- JS/description.js
+++ JS/description.js
@@ -1,4 +1,9 @@
-let comments = document.querySelector("#comments")
+let comments = document.querySelector("#comments");
+
+let divcard1 = document.querySelector("#divcard1");
+let divcard2 = document.querySelector("#divcard2");
+let divcard3 = document.querySelector("#divcard3");
+
 
 
  fetch("/JSON/description.json")
@@ -6,18 +11,15 @@ let comments = document.querySelector("#comments")
      .then(function (data) {
 
 
-        console.log(data[0].description);
-         comments.innerHTML = "<h2>"+ data[0].description +"</h2>";
-         comments.innerHTML += "<h2>"+ data[0].id +"</h2>";
-         comments.innerHTML += "<img src='" + data[0].url + " '>";
         
-         comments.innerHTML += "<h2>"+ data[0].description +"</h2>";
-         comments.innerHTML += "<h2>"+ data[0].id +"</h2>";
-         comments.innerHTML += "<img src='" + data[0].url + " '>";
+        divcard1.innerHTML += "<h5>"+ data[0].name +"</h5>";
+        divcard1.innerHTML += "<p>"+ data[0].comentario +"</p>";    
 
-         comments.innerHTML += "<h2>"+ data[0].description +"</h2>";
-         comments.innerHTML += "<h2>"+ data[0].id +"</h2>";
-         comments.innerHTML += "<img src='" + data[0].url + " '>";
+        divcard2.innerHTML += "<h5>"+ data[1].name +"</h5>";
+        divcard2.innerHTML += "<p>"+ data[1].comentario +"</p>"; 
+        
+        divcard3.innerHTML += "<h5>"+ data[2].name +"</h5>";
+        divcard3.innerHTML += "<p>"+ data[2].comentario +"</p>"; 
          
      })
 
